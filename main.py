61b371cfeab0852365759ee3a6cb98f856b65066
Will <whgriffiths1@sheffield.ac.uk>
2025-05-02 12:24:48 +0100

    refactored shipping fees

--- main.py
+++ main.py
@@ -40,16 +40,6 @@ class CustomerManager:
                 if total > 800:
                     print("Priority Customer")
 
-    def calculate_shipping_fee(self, purchases):
-        heavy_item = False
-        for purchase in purchases:
-            if purchase.get('weight', 0) > 20:
-                heavy_item = True
-                break
-        if heavy_item:
-            return 50
-        else:
-            return 20
 
 def calculate_shipping_fee_for_heavy_items(purchases):
     for purchase in purchases:
@@ -58,14 +48,9 @@ def calculate_shipping_fee_for_heavy_items(purchases):
     return 20
 
 def calculate_shipping_fee_for_fragile_items(purchases):
-    fragile_item = False
     for purchase in purchases:
         if purchase.get('fragile', False):
-            fragile_item = True
-            break
-    if fragile_item:
-        return 60
-    else:
-        return 25
+            return 60
+    return 25
 
 flat_tax = 0.2
